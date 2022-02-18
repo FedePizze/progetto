64fa5e92c9e17e23787abd62a450adc52f69d2a6
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-02-18 16:14:31 +0100

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <TitoloMio></TitoloMio>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TitoloMio from './components/TitoloMio.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+    TitoloMio
   }
 }
 </script>
